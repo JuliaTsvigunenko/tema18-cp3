ac3751cb5821b12afac0b459f056401b4d282490
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-02-29 21:04:18 +0200

    Add files via upload

---
+++ tema 18-cp3/ConsoleApp1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<Figure> figures = new List<Figure>();
+
+            while (true)
+            {
+                Console.Write("Введите фигуру: ");
+                string format = Console.ReadLine();
+                switch(format)
+                {
+
+                    case "1":
+                        Console.Write("Введите название: ");
+                        string nameR = Console.ReadLine();
+                        Console.Write("Введите сторону 1: ");
+                        double side1R = double.Parse(Console.ReadLine());
+                        Console.Write("Введите сторону 2: ");
+                        double side2R = double.Parse(Console.ReadLine());
+                        figures.Add(new Rectangle(nameR, side1R, side2R));
+                        break;
+
+
+                    case "2":
+                        Console.Write("Введите название: ");
+                        string nameS = Console.ReadLine();
+                        Console.Write("Введите сторону: ");
+                        double sideS = double.Parse(Console.ReadLine());
+                        figures.Add(new Square(nameS, sideS));
+                        break;
+
+
+
+                    case "3":
+                        Console.Write("Введите название: ");
+                        string nameT = Console.ReadLine();
+                        Console.Write("Введите сторону 1: ");
+                        double side1T = double.Parse(Console.ReadLine());
+                        Console.Write("Введите сторону 2: ");
+                        double side2T = double.Parse(Console.ReadLine());
+                        Console.Write("Введите сторону 3: ");
+                        double side3T = double.Parse(Console.ReadLine());
+                        figures.Add(new Triangle(nameT, side1T, side2T, side3T));
+                        break;
+
+
+                    case "4":
+                        Console.Write("Введите название: ");
+                        string nameC = Console.ReadLine();
+                        Console.Write("Введите радиус: ");
+                        double radiusC = double.Parse(Console.ReadLine());
+                        figures.Add(new Circle(nameC, radiusC));
+                        break;
+
+           
+                    default:
+                        Console.WriteLine("Неверно :(   Пожалуйста, попробуйте снова.");
+                        break;
+
+                }
+                Console.ReadKey();
+            }
+          
+
+
+
+
+
+
+
+
+
+
+           
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ tema 18-cp3/ConsoleApp1/Square.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // Производный класс квадрата
+    class Square : Figure
+    {
+
+        private double side;
+
+
+        /// <summary>
+        ///  // Конструктор с параметром для квадрата
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="side"></param>
+        public Square(string name, double side) : base(name)
+        {
+            this.side = side;
+        }
+
+        public double Side { get { return side; } set { side = value; } }
+
+
+
+        /// <summary>
+        ///  метод для вычисления площади квадрата
+        /// </summary>
+        /// <returns></returns>
+        public override double Area()
+        {
+            return side * side;
+        }
+
+
+
+        /// <summary>
+        ///  метод для вычисления периметра квадрата
+        /// </summary>
+        /// <returns></returns>
+        public override double Perimeter()
+        {
+            return 4 * side;
+        }
+
+
+        /// <summary>
+        ///  метод для вывода информации о квадрате
+        /// </summary>
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Сторона фигуры: {side}");
+        }
+
+
+
+
+
+
+
+    }
+}
